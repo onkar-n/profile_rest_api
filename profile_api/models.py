443f974343949535debc50cbb29f5f1ea1de9254
onkar-n <onkarnath05@gmail.com>
2020-07-01 13:38:51 +0530

    custom_user

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("User must have as email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    def create_superUser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superUser = True
+        user.staff = True
+        user.save(using=self._db)
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """DataBase model for profile"""
+    email = models.EmailField(max_length=125, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_fullName(self):
+        return self.name
+
+    def get_shortName(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
