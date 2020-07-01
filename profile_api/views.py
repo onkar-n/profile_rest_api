28a6ed4772dd2cac6eebf80cbe1894849b1cf410
onkar-n <onkarnath05@gmail.com>
2020-07-01 23:28:12 +0530

    auth

--- profile_api/views.py
+++ profile_api/views.py
@@ -19,19 +19,24 @@ class helloApiView(APIView):
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Helllo {name}'
-            return Response({'message':message})
+            return Response({'message':message}, content_type= "application/json")
         else:
             return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-
-
-
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
